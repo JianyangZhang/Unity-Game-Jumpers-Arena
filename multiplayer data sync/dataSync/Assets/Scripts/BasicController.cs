208e69d67ee44d95a1da35b474ade2e229fe6f38
JianyangZhang <jianyang212@outlook.com>
2017-01-25 09:31:15 -0800

    commits
    commits

---
+++ multiplayer data sync/dataSync/Assets/Scripts/BasicController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicController : MonoBehaviour {
+	public KeyCode upKey, downKey;
+	public float speed = 3;
+	private Rigidbody2D rigid;
+
+	void Start(){
+		rigid = GetComponent<Rigidbody2D>();
+	}
+	// Update is called once per frame
+	void Update() {
+		if (Input.GetKey(upKey)) {
+			rigid.velocity = new Vector2(0, speed);
+		} else {
+			rigid.velocity = new Vector2(0, 0);
+		}
+	}
+}
