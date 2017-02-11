93fe912d0ae9baa347760fb7ba0376aea536b56d
JianyangZhang <jianyang212@outlook.com>
2017-02-11 00:39:03 -0800

    commites
    commites

---
+++ single player basic/Test Project/Assets/SimpleFloor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleFloor : MonoBehaviour {
+	public Camera maincamera;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (maincamera.transform.position.y + (-5) > transform.position.y)
+			DestroyImmediate (this);
+	}
+}
