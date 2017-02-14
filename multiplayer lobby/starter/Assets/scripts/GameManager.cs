88fb84e858b7ec0e33f29f5a107eaa4737593aa9
JianyangZhang <jianyang212@outlook.com>
2017-02-14 14:45:17 -0800

    commites
    commites

---
+++ multiplayer lobby/starter/Assets/scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+	public static GameManager instance;
+	public string playerName;
+	public int characterIndex;
+	void Awake() {
+		if (instance) {
+			Destroy(gameObject);
+		} else {
+			instance = this;
+			playerName = "";
+			characterIndex = 0;
+			DontDestroyOnLoad(gameObject);
+		}
+	}
+
+	// Use this for initialization
+	void Start() {
+		
+	}
+	
+	// Update is called once per frame
+	void Update() {
+		
+	}
+}
---
+++ multiplayer lobby/starter/Assets/scripts/LobbySceneChange.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LobbySceneChange : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void pressBackBtn() {
+		SceneManager.LoadScene(0);
+	}
+}
