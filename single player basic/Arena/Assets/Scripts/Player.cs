92687e46b30f9630733961d3ef4604b2f31969e1
Xuyang Wang <xuyangw@usc.edu>
2017-02-18 22:03:15 -0800

    create new project

---
+++ single player basic/Arena/Assets/Scripts/Player.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public string id;
+    public Character character;
+    public string alias;
+    public int hp;
+    public List<Item> items;
+    public bool isShield;
+    public bool isAccelerated;
+    public bool isDecelerated;
+    public bool isStunned;
+
+    public Vector2 velocity
+    {
+        get
+        {
+            return GetComponent<Rigidbody2D>().velocity;
+        }
+        set
+        {
+            GetComponent<Rigidbody2D>().velocity = value;
+        }
+    }
+
+    public Vector2 location
+    {
+        get
+        {
+            return transform.position;
+        }
+        set
+        {
+            transform.position = value;
+        }
+    }
+
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
